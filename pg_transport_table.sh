4e11320757f3be0d306d8098b614de432fb3fa8c
Fujii Masao <fujii@postgresql.org>
2019-10-25 15:51:27 +0900

    Refactor the code to make it simpler.

--- pg_transport_table.sh
+++ pg_transport_table.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 PROGNAME=$(basename ${0})
 CURDIR=$(pwd)
 
@@ -13,13 +15,6 @@ elog ()
 {
   echo "$PROGNAME: ERROR: $1" 1>&2
   exit 1
-}
-
-exit_on_error ()
-{
-  if [ $? -ne 0 ]; then
-    exit 1
-  fi
 }
 
 usage ()
@@ -64,94 +59,99 @@ if [ -z "$TBL" ]; then
   elog "name of table to transport must be specified"
 fi
 
-SSYSID=$($PSQL "$SRC" -c "SELECT system_identifier FROM pg_control_system()")
-exit_on_error
-DSYSID=$($PSQL "$DST" -c "SELECT system_identifier FROM pg_control_system()")
-exit_on_error
+RETSRC=
+psql_src ()
+{
+  [ ! -z "$1" ] && SQL="$1"
+  RETSRC=$($PSQL -d "$SRC" -c "$SQL")
+}
 
-if [ "$SSYSID" == "$DSYSID" ]; then
-  elog "source and destination servers must be differenet"
-fi
+RETDST=
+psql_dst ()
+{
+  [ ! -z "$1" ] && SQL="$1"
+  RETDST=$($PSQL -d "$DST" -c "$SQL")
+}
 
-STBL=$($PSQL "$SRC" -c "SELECT relfilenode FROM pg_class WHERE relname = '$TBL'")
-exit_on_error
-if [ -z "$STBL" ]; then
-  elog "table \"$TBL\" doesn't exist in source server"
-fi
-DTBL=$($PSQL "$DST" -c "SELECT relfilenode FROM pg_class WHERE relname = '$TBL'")
-exit_on_error
-if [ -z "$DTBL" ]; then
-  elog "table \"$TBL\" doesn't exist in destination server"
-fi
-echo "mv $STBL $DTBL"
-echo "mv ${STBL}_fsm ${DTBL}_fsm"
-echo "mv ${STBL}_vm ${DTBL}_vm"
-cat <<EOF
-for num in \$(seq 1 100000); do
-  if [ -f "${STBL}.\${num}" ]; then
-    mv ${STBL}.\${num} ${DTBL}.\${num}
-  else
-    break
-  fi
-done
-EOF
+psql_both ()
+{
+  [ ! -z "$1" ] && SQL="$1"
+  psql_src
+  psql_dst
+}
 
-IDXLIST=$($PSQL "$SRC" -c "SELECT indexname FROM pg_indexes WHERE tablename = '$TBL'")
-exit_on_error
-for idx in $IDXLIST; do
-  SIDX=$($PSQL "$SRC" -c "SELECT relfilenode FROM pg_class WHERE relname = '$idx'")
-  exit_on_error
-  if [ -z "$SIDX" ]; then
-    elog "index \"$idx\" doesn't exist in source server"
+check_sysid ()
+{
+  psql_both "SELECT system_identifier FROM pg_control_system()"
+  if [ "$RETSRC" == "$RETDST" ]; then
+    elog "source and destination servers must be differenet"
   fi
-  DIDX=$($PSQL "$DST" -c "SELECT relfilenode FROM pg_class WHERE relname = '$idx'")
-  exit_on_error
-  if [ -z "$DIDX" ]; then
-    elog "index \"$idx\" doesn't exist in destination server"
-  fi
-  echo "mv $SIDX $DIDX"
+}
+check_sysid
+
+rename_multiple_files ()
+{
+  FILESRC="$1"
+  FILEDST="$2"
+  MAXNUMFILES=100000
   cat <<EOF
-for num in \$(seq 1 100000); do
-  if [ -f "${SIDX}.\${num}" ]; then
-    mv ${SIDX}.\${num} ${DIDX}.\${num}
+for num in \$(seq 1 $MAXNUMFILES); do
+  if [ -f "${FILESRC}.\${num}" ]; then
+    mv ${FILESRC}.\${num} ${FILEDST}.\${num}
   else
     break
   fi
 done
 EOF
-done
+}
 
-STOAST=$($PSQL "$SRC" -c "SELECT reltoastrelid FROM pg_class WHERE relname = '$TBL'")
-exit_on_error
-DTOAST=$($PSQL "$DST" -c "SELECT reltoastrelid FROM pg_class WHERE relname = '$TBL'")
-exit_on_error
-if [ $STOAST -ne 0 ]; then
-  if [ $DTOAST -eq 0 ]; then
-    elog "table \"$TBL\" doesn't have TOAST table in destination server"
-  fi
-  echo "mv $STOAST $DTOAST"
-  cat <<EOF
-for num in \$(seq 1 100000); do
-  if [ -f "${STOAST}.\${num}" ]; then
-    mv ${STOAST}.\${num} ${DTOAST}.\${num}
-  else
-    break
-  fi
-done
-EOF
+rename_table ()
+{
+  psql_both "SELECT relfilenode FROM pg_class WHERE relname = '$TBL'"
+  [ -z "$RETSRC" ] && elog "table \"$TBL\" doesn't exist in source server"
+  [ -z "$RETDST" ] && elog "table \"$TBL\" doesn't exist in destination server"
 
-  STOASTIDX=$($PSQL "$SRC" -c "SELECT indexrelid FROM pg_index WHERE indrelid = $STOAST")
-  exit_on_error
-  DTOASTIDX=$($PSQL "$DST" -c "SELECT indexrelid FROM pg_index WHERE indrelid = $DTOAST")
-  exit_on_error
-  echo "mv $STOASTIDX $DTOASTIDX"
-  cat <<EOF
-for num in \$(seq 1 100000); do
-  if [ -f "${STOASTIDX}.\${num}" ]; then
-    mv ${STOASTIDX}.\${num} ${DTOASTIDX}.\${num}
-  else
-    break
-  fi
-done
-EOF
-fi
+  echo "mv $RETSRC $RETDST"
+  echo "rm ${RETSRC}_init"
+  echo "mv ${RETSRC}_fsm ${RETDST}_fsm"
+  echo "mv ${RETSRC}_vm ${RETDST}_vm"
+  rename_multiple_files "$RETSRC" "$RETDST"
+}
+rename_table
+
+rename_indexes ()
+{
+  psql_src "SELECT indexname FROM pg_indexes WHERE tablename = '$TBL'"
+  IDXLIST="$RETSRC"
+  for idx in $IDXLIST; do
+    psql_both "SELECT relfilenode FROM pg_class WHERE relname = '$idx'"
+    [ -z "$RETSRC" ] && elog "index \"$idx\" doesn't exist in source server"
+    [ -z "$RETDST" ] && elog "index \"$idx\" doesn't exist in destination server"
+
+    echo "mv $RETSRC $RETDST"
+    echo "rm ${RETSRC}_init"
+    rename_multiple_files "$RETSRC" "$RETDST"
+  done
+}
+rename_indexes
+
+rename_toast ()
+{
+  psql_both "SELECT reltoastrelid FROM pg_class WHERE relname = '$TBL'"
+  [ $RETSRC -eq 0 ] && return 0
+  [ $RETDST -eq 0 ] && elog "table \"$TBL\" doesn't have TOAST table in destination server"
+
+  echo "mv $RETSRC $RETDST"
+  echo "rm ${RETSRC}_init"
+  rename_multiple_files "$RETSRC" "$RETDST"
+}
+rename_toast
+
+rename_toast_index ()
+{
+  psql_both "SELECT indexrelid FROM pg_class, pg_index WHERE relname = '$TBL' AND reltoastrelid = indrelid"
+  echo "mv $RETSRC $RETDST"
+  echo "rm ${RETSRC}_init"
+  rename_multiple_files "$RETSRC" "$RETDST"
+}
+rename_toast_index
