1e550d5b04dc33c1dc99c5c0383365115c80f8c9
Fujii Masao <fujii@postgresql.org>
2020-02-20 22:23:09 +0900

    First commit of pg_transport_table.sql.

---
+++ pg_transport_table.sql
@@ -0,0 +1,81 @@
+CREATE OR REPLACE FUNCTION print_transport_commands (
+    tbl regclass,
+    newtblid bigint,
+    newtoastid bigint DEFAULT 0,
+    newtoastidxid bigint DEFAULT 0)
+    RETURNS SETOF text AS $$
+DECLARE
+    basedir text;
+    oldtblid bigint := 0;
+    oldtoastid bigint := 0;
+    oldtoastidxid bigint := 0;
+    oldreltoastrelid bigint := 0;
+    segno bigint;
+    filepath text;
+    fork text;
+BEGIN
+    IF newtblid = newtoastid THEN
+        RAISE EXCEPTION 'newtblid must be different from newtoastid';
+    ELSIF newtblid = newtoastidxid THEN
+        RAISE EXCEPTION 'newtblid must be different from newtoastidxid';
+    ELSIF newtoastid = 0 AND newtoastidxid <> 0 THEN
+        RAISE EXCEPTION 'newtoastidxid must be specified together if newtoastid is specified';
+    ELSIF newtoastid <> 0 AND newtoastidxid = 0 THEN
+        RAISE EXCEPTION 'newtoastid must be specified together if newtoastidxid is specified';
+    END IF;
+
+    basedir := rtrim(pg_relation_filepath(tbl), '0123456789');
+    oldtblid := pg_relation_filenode(tbl);
+    RETURN NEXT 'mv ' || basedir || oldtblid || ' ' || basedir || newtblid;
+
+    segno := 1;
+    LOOP
+        filepath := basedir || oldtblid || '.' || segno;
+	EXIT WHEN pg_stat_file(filepath, true) IS NULL;
+	RETURN NEXT 'mv ' || filepath || ' ' || basedir || newtblid || '.' || segno;
+	segno := segno + 1;
+    END LOOP;
+
+    FOR fork IN SELECT unnest(ARRAY['_fsm', '_vm', '_init']) LOOP
+        filepath := basedir || oldtblid || fork;
+	CONTINUE WHEN pg_stat_file(filepath, true) IS NULL;
+        RETURN NEXT 'mv ' || filepath || ' ' || basedir || newtblid || fork;
+    END LOOP;
+
+    IF newtoastid = 0 THEN
+        RETURN;
+    END IF;
+
+    SELECT reltoastrelid INTO oldreltoastrelid FROM pg_class WHERE oid = tbl;
+    IF oldreltoastrelid = 0 THEN
+        RAISE EXCEPTION 'TOAST must exist if newtoastid is specified';
+    END IF;
+
+    oldtoastid := pg_relation_filenode(oldreltoastrelid);
+    SELECT pg_relation_filenode(indexrelid) INTO oldtoastidxid FROM pg_index
+        WHERE indrelid = oldreltoastrelid;
+    IF oldtoastidxid IS NULL THEN
+        RAISE EXCEPTION 'TOAST index must exist if TOAST exists';
+    END IF;
+
+    RETURN NEXT 'mv ' || basedir || oldtoastid || ' ' || basedir || newtoastid;
+    segno := 1;
+    LOOP
+        filepath := basedir || oldtoastid || '.' || segno;
+	EXIT WHEN pg_stat_file(filepath, true) IS NULL;
+	RETURN NEXT 'mv ' || filepath || ' ' || basedir || newtoastid || '.' || segno;
+	segno := segno + 1;
+    END LOOP;
+
+    RETURN NEXT 'mv ' || basedir || oldtoastidxid || ' ' || basedir || newtoastidxid;
+    segno := 1;
+    LOOP
+        filepath := basedir || oldtoastidxid || '.' || segno;
+	EXIT WHEN pg_stat_file(filepath, true) IS NULL;
+	RETURN NEXT 'mv ' || filepath || ' ' || basedir || newtoastidxid || '.' || segno;
+	segno := segno + 1;
+    END LOOP;
+
+    RETURN;
+END;
+$$ LANGUAGE plpgsql STRICT VOLATILE;
